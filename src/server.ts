2fa0f54ae521ac5ba85cab3a7347a73e6eb914ff
SeungUkLee <lsy931106@gmail.com>
2020-04-11 23:10:50 +0900

    Add: round3

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
 import * as express from "express";
 import * as morgan from "morgan";
+import { router } from "./routes";
+
 
 const app = express();
 
 app.use(morgan('dev'));
-
-app.get("/articles", (req, res) => {
-    res.json({}).status(200)
-});
+app.use(router);
 
 app.listen(3000, () => console.log("[SERVER] running on 3000..."));
 
